8e0aad90c88c98c8d8a06c683f83b095468a401f
Nitin Chaurasia <gs.nitin@gmail.com>
2024-04-02 19:36:48 -0600

    added time

--- src/main/java/org/learn/HelloWorld.java
+++ src/main/java/org/learn/HelloWorld.java
@@ -1,7 +1,11 @@
 package org.learn;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!" + ZonedDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS a z(O)")));
     }
 }
